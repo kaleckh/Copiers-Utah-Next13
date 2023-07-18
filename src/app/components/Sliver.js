aa8df4bb28c5673083a1280311d76bde87e8c6b1
kaleckh <69163050+kaleckh@users.noreply.github.com>
2023-07-18 15:57:37 -0600

    fixed case issue

--- src/app/components/Sliver.js
+++ src/app/components/Sliver.js
@@ -4,7 +4,8 @@ import React, {
     useImperativeHandle,
     useRef,
     useEffect,
-  } from 'react'
+} from 'react'
+import Image from 'next/image';
 //   import { MDBInput, MDBTextArea } from 'mdb-react-ui-kit'
   import Axios from 'axios'
   import ReCAPTCHA from 'react-google-recaptcha'
@@ -12,13 +13,15 @@ import React, {
   
   const Sliver = () => {
     return (
-      <div style={{display:"flex", alignItems:"center"}}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        
         <div className={styles.sliver}>
           Dont Know Where To Start?{' '}
           <a style={{ paddingLeft: '15px', color:"white" }} href="tel:8012610510">
             {' '}
             Call Us At (801)-261 0510
           </a>
+          <Image src={'/static/facebook.webp'} width={20} height={20}/>
         </div>
       </div>
     )
