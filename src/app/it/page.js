5ba34b509d43138dcacf528acf1e20d0f2ca6eac
kaleckh <69163050+kaleckh@users.noreply.github.com>
2023-07-19 11:22:40 -0600

    changed case settings

---
+++ src/app/it/page.js
@@ -0,0 +1,14 @@
+import Data from './dataa'
+export const metadata = {
+  title: 'IT Services and Support | Get a Quote Today | Copiers Utah',
+  description: 'Copiers Utah provides comprehensive IT services and support. Fill out our easy form to get a personalized quote and get the IT help you need',
+  keywords: "IT services, IT support, IT help, IT quote, copiers Utah"
+}
+const it = () => {
+
+  return (
+    <Data />
+  )
+}
+
+export default it
---
+++ src/app/konika/page.js
@@ -0,0 +1,14 @@
+import Data from './dataa'
+export const metadata = {
+  title: 'New and Used Konika Copiers | High-Quality and Affordable | Copiers Utah',
+  description: 'Copiers Utah offers high-quality and affordable Konika copiers, both new and used. Learn more about Konika copiers and how they can benefit your office. Fill out our quote form to receive a customized quote',
+  keywords:"Lexmark copiers, used Lexmark copiers, new Lexmark copiers, office copiers, copiers Utah, affordable copiers"
+}
+const konika = () => {
+
+  return (
+    <Data />
+  )
+}
+
+export default konika
