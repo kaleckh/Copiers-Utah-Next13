800adc00235d945d7dc122ed713d8c4f46cdb54b
kaleckh <69163050+kaleckh@users.noreply.github.com>
2023-09-14 13:11:02 -0600

    styling

--- src/app/toner/dataa.js
+++ src/app/toner/dataa.js
@@ -104,136 +104,142 @@ const Buy = (props) => {
       <Header />
       <BreadCrumbs breadCrumbs={breadCrumbs} />
       <div className={styles.mainContent}>
-        <div className={styles.filter}>
-          <div className={styles.centerNormal}>
-            <div className={styles.titleBig}>Filters</div>
-          </div>
-          <div className={styles.columnCenter}>
+        <div style={{ width: "80%", display: "flex" }}>
+          <div className={styles.filter}>
             <div className={styles.centerNormal}>
-              <div className={styles.titleSmall}>Brands</div>
-              <div className={styles.lineSmall}></div>
+              <div style={{ fontSize: "18px", width: "70%", fontWeight: "400" }} className={styles.titleBig}>Filter</div>
             </div>
-            <div className={styles.row}>
-              <input type="checkbox" />
-              <div>Epson</div>
-            </div>
-            <div className={styles.row}>
-              <input type="checkbox" />
-              <div>Konika Minolta</div>
-            </div>
-            <div className={styles.row}>
-              <input type="checkbox" />
-              <div>HP</div>
-            </div>
-            <div className={styles.row}>
-              <input type="checkbox" />
-              <div>Xenox</div>
-            </div>
-          </div>
-          <div className={styles.columnCenter}>
-            <div className={styles.titleBig}>color</div>
-            <div className={styles.row}>
-              <input type="checkbox" />
-              <div>Black</div>
-            </div>
-            <div className={styles.row}>
-              <input type="checkbox" />
-              <div>Blue</div>
-            </div>
-            <div className={styles.row}>
-              <input type="checkbox" />
-              <div>Red</div>
-            </div>
-            <div className={styles.row}>
-              <input type="checkbox" />
-              <div>Yellow</div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.center}>
-          <div className={styles.boxContainer}>
-            {toners.map((toner) => {
-              return (
-                <div
-                  key={toner.oem}
-                  // onClick={() => {
-                  //   setCartLook({
-                  //     name: toner.name,
-                  //     oem: toner.oem,
-                  //     price: toner.price,
-                  //     color: toner.color,
-                  //     photo: toner.image,
-                  //     yield: toner.yield,
-                  //   });
-                  // }}
-                  className={styles.box}
-                >
-
-                  <Link
-                    onClick={() => {
-                      setTonerOem(toner.oem)
-                      localStorage.setItem("tonerOem", toner.oem)
-
-                    }}
-                    className={styles.somethingElse}
-                    href={`/tonerChoice?oem=${toner.oem}`}
-                  >
-                    <Image
-                      style={{ borderRadius: "5px" }}
-                      src={toner.image}
-                      width={180}
-                      height={180}
-                    ></Image>
-                    <div className={styles.titleSmallBlack}>{toner.name}</div>
-                    <div style={{ width: "100%" }}>
-                      <div className={styles.row}>
-                        <div className={styles.row}>
-                          <div
-                            style={{
-                              display: "flex",
-                              justifyContent: "center",
-                              alignItems: "center",
-                            }}
-                          >
-                            <div
-
-                              style={{ paddingRight: "5px", color: "rgb(2,50,92)" }}
-                              className={styles.price}
-                            >
-                              $
-                            </div>
-                            <div style={{ color: "rgb(2,50,92)" }} className={styles.modelSmallish}>
-                              {toner.price}
-                            </div>
-                          </div>
-                        </div>
-                        <div className={styles.row}>
-                          <div
-                            style={{ paddingRight: "8px" }}
-                            className={styles.priceSmall}
-                          >
-                            OEM:
-                          </div>
-                          <div className={styles.modelSmall}>{toner.oem}</div>
-                        </div>
-                      </div>
-                      <div
-                        style={{ paddingTop: "10px" }}
-                        className={styles.row}
-                      >
-                        <div
-                          style={{ paddingRight: "8px", paddingBottom: "5px" }}
-                          className={styles.priceMedium}
-                        >
-                          Models:
-                        </div>
-                        <div className={styles.modelSmall}>{toner.models}</div>
-                      </div>
-                    </div>
-                  </Link>
+            <div className={styles.columnCenter}>
+              <div className={styles.centerNormal}>
+                <div style={{ fontSize: "15px", width: "100%", fontWeight: "300" }} className={styles.titleSmall}>Brands</div>
+              </div>
+              <div className={styles.row}>
+                <div className={styles.width}>
+                  <input type="checkbox" />
                 </div>
-              );
-            })}
+                <div>
+                  <div>Epson</div>
+                </div>
+              </div>
+              <div className={styles.row}>
+                <input type="checkbox" />
+                <div>Konika Minolta</div>
+              </div>
+              <div className={styles.row}>
+                <input type="checkbox" />
+                <div>HP</div>
+              </div>
+              <div className={styles.row}>
+                <input type="checkbox" />
+                <div>Xenox</div>
+              </div>
+            </div>
+            <div className={styles.columnCenter}>
+              <div className={styles.titleBig}>color</div>
+              <div className={styles.row}>
+                <input type="checkbox" />
+                <div>Black</div>
+              </div>
+              <div className={styles.row}>
+                <input type="checkbox" />
+                <div>Blue</div>
+              </div>
+              <div className={styles.row}>
+                <input type="checkbox" />
+                <div>Red</div>
+              </div>
+              <div className={styles.row}>
+                <input type="checkbox" />
+                <div>Yellow</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.center}>
+            <div className={styles.boxContainer}>
+              {toners.map((toner) => {
+                return (
+                  <div
+                    key={toner.oem}
+                    // onClick={() => {
+                    //   setCartLook({
+                    //     name: toner.name,
+                    //     oem: toner.oem,
+                    //     price: toner.price,
+                    //     color: toner.color,
+                    //     photo: toner.image,
+                    //     yield: toner.yield,
+                    //   });
+                    // }}
+                    className={styles.box}
+                  >
+
+                    <Link
+                      onClick={() => {
+                        setTonerOem(toner.oem)
+                        localStorage.setItem("tonerOem", toner.oem)
+
+                      }}
+                      className={styles.somethingElse}
+                      href={`/tonerChoice?oem=${toner.oem}`}
+                    >
+                      <Image
+                        style={{ borderRadius: "5px" }}
+                        src={toner.image}
+                        width={180}
+                        height={180}
+                      ></Image>
+                      <div className={styles.titleSmallBlack}>{toner.name}</div>
+                      <div style={{ width: "100%" }}>
+                        <div style={{ paddingRight: "15px" }} className={styles.row}>
+                          <div className={styles.row}>
+                            <div
+                              style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                              }}
+                            >
+                              <div
+
+                                style={{ paddingRight: "5px", color: "rgb(2,50,92)" }}
+                                className={styles.price}
+                              >
+                                $
+                            </div>
+                              <div style={{ color: "rgb(2,50,92)" }} className={styles.modelSmallish}>
+                                {toner.price}
+                              </div>
+                            </div>
+                          </div>
+                          <div className={styles.row}>
+                            <div
+                              style={{ paddingRight: "5px" }}
+                              className={styles.priceSmall}
+                            >
+                              OEM:
+                          </div>
+                            <div className={styles.modelSmall}>{toner.oem}</div>
+                          </div>
+                        </div>
+                        <div
+                          style={{ paddingTop: "10px" }}
+                          className={styles.rowOem}
+                        >
+                          <div
+                            style={{ paddingRight: "8px", paddingBottom: "5px" }}
+                            className={styles.priceMedium}
+                          >
+                            Models:
+                        </div>
+                          <div className={styles.modelSmall}>{toner.models}</div>
+
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
